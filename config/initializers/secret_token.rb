4ee1030984e884fa396d53f874ce67aa921a4d7a
cleaner <git@drd2.org>
2011-06-14 20:05:03 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'f8eae46fdf926e1466210ea264cd7141c476c3950807e49e94fb4d586549ac67e265008a0ad8be5e2b94e2f82b0ae358432dc5788c963b1bfca8b8bff5558636'
